786c3da950ad1006b6cdfb79e1aa4f4f4f43eb67
SSwensen <scottswensen23@gmail.com>
2017-02-13 14:26:54 -0700

    Added baseline files

---
+++ src/Model/TestModel.java
@@ -0,0 +1,7 @@
+package Model;
+
+/**
+ * Created by SummitDrift on 2/13/17.
+ */
+public class TestModel {
+}
---
+++ src/View/View.java
@@ -0,0 +1,7 @@
+package View;
+
+/**
+ * Created by SummitDrift on 2/13/17.
+ */
+public class View {
+}
