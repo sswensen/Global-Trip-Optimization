70b6fe896d1a6149a23c57752f0801a8740f82f4
chundrus <schundru1@gmail.com>
2017-02-15 13:46:36 -0700

    working on addlabel()

--- src/View/View.java
+++ src/View/View.java
@@ -3,13 +3,63 @@ package View;
 /**
  * Created by SummitDrift on 2/13/17.
  */
+import java.io.File;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.DOMImplementation;
+
 public class View {
+	public DocumentBuilderFactory factory;
+	public DocumentBuilder docbuilder;
+	
+	// root elements
+	public Document XMLdoc;
+	public Element  rootElement;
+	
 	public void initializeTrip(){
-		
+		factory = DocumentBuilderFactory.newInstance(); 
+		docbuilder = factory.newDocumentBuilder();
+		XMLdoc = docBuilder.newDocument();
+		rootElement = doc.createElement("trip");
+		doc.appendChild(rootElement);
 	}
 	
-	public void addLeg(int a, String b,String c, String d){
+	public void addLeg(String sequence,String start, String finish, int mileage){
+		//leg grouping
+		Element leg = XMLdoc.createElement("leg");
+		rootElement.appendChild(leg);
 		
+		//Sequence element
+		Element sequence = XMLdoc.createElement("sequence");
+		sequence.appendChild(doc.createTextNode(sequence));
+		leg.appendChild(sequence);
+		
+		//Start element
+		Element start = XMLdoc.createElement("start");
+		start.appendChild(doc.createTextNode(start));
+		leg.appendChild(start);
+		
+		//finish element
+		Element finish = XMLdoc.createElement("finish");
+		finish.appendChild(doc.createTextNode(finish));
+		leg.appendChild(finish);
+				
+		//mileage element
+		Element mileage = XMLdoc.createElement("mileage");
+		mileage.appendChild(doc.createTextNode(mileage));
+		leg.appendChild(mileage);
 	}
 	
 	public void addLline(double longtitude1, latitude1, longitude2, latitude2){
@@ -30,5 +80,10 @@ public class View {
 	
 	public void finalizeTrip(){
 		
+	}
+	
+	public static void main(String argv[]) {
+		View map = new View();
+		map.initializeTrip();
 	}
 }
