c367e913b3ae29d8d9df3b81059625d07406dfd5
jzuniga77 <jzuniga7@rams.colostate.edu>
2017-02-17 12:44:33 -0700

    Added main in Presenter.java, testing IntelliJ/GitHub integration.

--- src/Presenter/Presenter.java
+++ src/Presenter/Presenter.java
@@ -3,5 +3,10 @@ package Presenter;
 /**
  * Created by SummitDrift on 2/13/17.
  */
-public class Presenter {
+public class Presenter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("HAY");
+    }
 }
