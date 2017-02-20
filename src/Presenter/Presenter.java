e4b4298c099369136061c6fd01cf18658ad02988
jzuniga77 <jzuniga7@rams.colostate.edu>
2017-02-20 14:24:56 -0700

    Started init() to glue model to view

--- src/Presenter/Presenter.java
+++ src/Presenter/Presenter.java
@@ -3,6 +3,7 @@ package Presenter;
 import Model.*;
 import View.*;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileNotFoundException;
 
 public class Presenter
@@ -16,19 +17,28 @@ public class Presenter
         this.view = view;
     }
 
-    public void planTrip()
+    public void init(String filename) throws FileNotFoundException
     {
-
+        model.planTrip(filename);
+        int numPairs = model.getNumPairs();
+        for(int i=0; i<numPairs; i++)
+        {
+            double firstLon = model.getFirstLon(i);
+            double firstLat = model.getFirstLat(i);
+            double secondLon = model.getSecondLon(i);
+            double secondLat = model.getSecondLat(i);
+            int pairDistance = model.getPairDistance(i);
+        }
     }
 
-    public static void main(String[] args) throws FileNotFoundException
+    public static void main(String[] args) throws FileNotFoundException, ParserConfigurationException
     {
         String filename = args[0];
         String option = args[1];
         Model model = new Model();
         View view = new View();
         Presenter presenter = new Presenter(model, view);
-        presenter.model.planTrip(filename);
-
+        presenter.init(filename);
+        presenter.view.initializeTrip();
     }
 }
