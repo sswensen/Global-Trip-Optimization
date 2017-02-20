fac3d8ebb6e7a2e25aa9221dfc2da2707e76b5a6
jzuniga77 <jzuniga7@rams.colostate.edu>
2017-02-20 13:19:22 -0700

    Added initial testing of model and view to presenter

--- src/Presenter/Presenter.java
+++ src/Presenter/Presenter.java
@@ -1,12 +1,29 @@
 package Presenter;
 
-/**
- * Created by SummitDrift on 2/13/17.
- */
+import Model.*;
+import View.*;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+
 public class Presenter
 {
+    private Model model;
+    private View view;
+
+    public Presenter(Model model, View view)
+    {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void planTrip()
+    {
+
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("HAY");
+        Model model = new Model();
+        View view = new View();
+        Presenter presenter = new Presenter(model, view);
     }
 }
