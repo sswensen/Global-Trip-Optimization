853eb58485fb524b8c8a72fb89f92e0f34cdb2ca
chundrus <chundrus@rams.colostate.edu>
2017-02-21 01:39:35 -0700

    implemented testing for view class

--- src/View/TestView.java
+++ src/View/TestView.java
@@ -1,7 +1,106 @@
 package View;
 
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by SummitDrift on 2/13/17.
  */
 public class TestView {
+    @Before
+    public void initalize() {
+
+    }
+    private View v = new View();
+
+    @Test
+    public void initializeTrip() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc(), v.getXMLdoc());
+    }
+
+    @Test
+    public void addLeg() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc().getFirstChild(), v.getXMLdoc().getFirstChild());
+    }
+
+    @Test
+    public void addFinalLeg() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc().getFirstChild(), v.getXMLdoc().getFirstChild());
+    }
+
+    @Test
+    public void addLine() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc().getFirstChild(), v.getXMLdoc().getFirstChild());
+    }
+
+    @Test
+    public void addDistance() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc().getFirstChild(), v.getXMLdoc().getFirstChild());
+    }
+
+    @Test
+    public void addCityNameLabel() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc().getFirstChild(), v.getXMLdoc().getFirstChild());
+    }
+
+    @Test
+    public void addIDLabel() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc().getFirstChild(), v.getXMLdoc().getFirstChild());
+    }
+
+    @Test
+    public void addHeader() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc().getFirstChild(), v.getXMLdoc().getFirstChild());
+    }
+
+    @Test
+    public void addFooter() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc().getFirstChild(), v.getXMLdoc().getFirstChild());
+    }
+
+    @Test
+    public void addBorders() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc().getFirstChild(), v.getXMLdoc().getFirstChild());
+    }
+
+    @Test
+    public void finalizeTrip() throws Exception {
+        v.initializeTrip();
+        View one = new View();
+        one.initializeTrip();
+        assertEquals(one.getXMLdoc().getFirstChild(), v.getXMLdoc().getFirstChild());
+    }
+
 }
