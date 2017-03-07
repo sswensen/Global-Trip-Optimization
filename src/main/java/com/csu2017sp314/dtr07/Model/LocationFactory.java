1177d04e299cdf3feefb0ad6b01914d78d1b34dd
jzuniga77 <jzuniga7@rams.colostate.edu>
2017-03-07 16:00:42 -0700

    Did some revisions in LocationFactory to improve code climate score.

--- src/main/java/com/csu2017sp314/dtr07/Model/LocationFactory.java
+++ src/main/java/com/csu2017sp314/dtr07/Model/LocationFactory.java
@@ -11,8 +11,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class LocationFactory {
-    //private static final char DEFAULT_SEPARATOR = ',';
-    //private static final char DEFAULT_QUOTE = '\"';
     private ArrayList<Location> locations = new ArrayList<Location>();
     private ArrayList<Pair> pairs = new ArrayList<>();
     private ArrayList<Pair> bestPairs = new ArrayList<>();
@@ -43,35 +41,12 @@ class LocationFactory {
         }
         while (scan.hasNext()) {
             String[] line = scan.nextLine().split(",");
-            //System.out.println("[id= " + line[0] + ", name= " + line[1] + " , city=" + line[2] + " , lat=" + line[3] + " , lon=" + line[4] + " , alt=" + line[5] + "]");
             Location temp = new Location(line[id], line[name], line[latitude].replaceAll("\\s+",""), line[longitude].replaceAll("\\s+",""));
             locations.add(temp);
         }
         scan.close();
         return locations.size() > 0;
     }
-
-    /*
-    boolean secondTry() {
-        for(int x = 0; x < locations.size()-1; x++) {
-            double distance = 999999999;
-            int index = -1;
-            for(int y = x+1; y < locations.size(); y++) {
-                double temp = locations.get(x).distance(locations.get(y));
-                if(distance > temp) {
-                    distance = temp;
-                    index = y;
-                }
-            }
-            Location temploc = locations.get(x+1);
-            locations.set(x+1, locations.get(index));
-            locations.set(index, temploc);
-            pairs.add(new Pair(x, locations.get(x), locations.get(x+1), locations.get(x).distance(locations.get(x+1))));
-        }
-        pairs.add(new Pair(locations.size()-1, locations.get(locations.size()-1), locations.get(0), locations.get(locations.size()-1).distance(locations.get(0))));
-        return true;
-    }
-    */
 
     boolean thirdTry() {
         int bestDistance = 999999999;
@@ -113,54 +88,6 @@ class LocationFactory {
         return true;
     }
 
-    /*
-    boolean findNearest() {
-        boolean ret = false;
-        double distance;
-        int s1 = -1;
-        int s2 = -1;
-        int ind = 0;
-        for(int i = 0; i < locations.size(); i++) {
-            distance = 9999999;
-            for(int j = 0; j < locations.size(); j++) {
-                if(i != j) {
-                    double temp = Math.abs(locations.get(i).distance(locations.get(j)));
-                    if(temp < distance) {
-                        distance = temp;
-                        s1 = i;
-                        s2 = j;
-                        ret = true;
-                    }
-                }
-            }
-            //add nearest to
-            if(s1 < locations.size()) {
-                if(locations.get(s2).getNearest() < 0) {
-                    if (locations.get(s1).getNearestDistance() > distance) {
-                        locations.get(s1).setNearest(locations.get(s2).getId());
-                        locations.get(s1).setNearestDistance((int) Math.round(distance));
-                        pairs.add(new Pair(++ind, locations.get(s1), locations.get(s2), Math.round(distance)));
-                    } else {
-                        System.out.println("UHHHHHHHHHHHHHHHHHHHHHHHHh");
-                    }
-                    Location tempLoc = locations.get(s1 + 1);
-                    locations.set(s1 + 1, locations.get(s2));
-                    locations.set(s2, tempLoc);
-                } else {
-                    locations.get(s1).setNearest(locations.get(s2).getId());
-                    locations.get(s1).setNearestDistance((int) Math.round(distance));
-                    pairs.add(new Pair(++ind, locations.get(s1), locations.get(s2), Math.round(distance)));
-                }
-            } else {
-                locations.get(s1).setNearest(locations.get(s2).getId());
-                locations.get(s1).setNearestDistance((int) Math.round(distance));
-                pairs.add(new Pair(++ind, locations.get(s1), locations.get(s2), Math.round(distance)));
-            }
-        }
-        return ret;
-    }
-    */
-
     ArrayList<Location> getLocations() {
         return locations;
     }
@@ -168,73 +95,4 @@ class LocationFactory {
     ArrayList<Pair> getPairs() {
         return pairs;
     }
-
-    /*
-    private static List<String> parseLine(String cvsLine) {
-        return LocationFactory.parseLine(cvsLine, ',', '\"');
-    }
-
-    private static List<String> parseLine(String cvsLine, char separators) {
-        return LocationFactory.parseLine(cvsLine, separators, '\"');
-    }
-
-    private static List<String> parseLine(String cvsLine, char separators, char customQuote) {
-        char[] chars;
-        ArrayList<String> result = new ArrayList<String>();
-        if (cvsLine == null && cvsLine.isEmpty()) {
-            return result;
-        }
-        if (customQuote == ' ') {
-            customQuote = (char)34;
-        }
-        if (separators == ' ') {
-            separators = (char)44;
-        }
-        StringBuffer curVal = new StringBuffer();
-        boolean inQuotes = false;
-        boolean startCollectChar = false;
-        boolean doubleQuotesInColumn = false;
-        for (char ch : chars = cvsLine.toCharArray()) {
-            if (inQuotes) {
-                startCollectChar = true;
-                if (ch == customQuote) {
-                    inQuotes = false;
-                    doubleQuotesInColumn = false;
-                    continue;
-                }
-                if (ch == '\"') {
-                    if (doubleQuotesInColumn) continue;
-                    curVal.append(ch);
-                    doubleQuotesInColumn = true;
-                    continue;
-                }
-                curVal.append(ch);
-                continue;
-            }
-            if (ch == customQuote) {
-                inQuotes = true;
-                if (chars[0] != '\"' && customQuote == '\"') {
-                    curVal.append('\"');
-                }
-                if (!startCollectChar) continue;
-                curVal.append('\"');
-                continue;
-            }
-            if (ch == separators) {
-                result.add(curVal.toString());
-                curVal = new StringBuffer();
-                startCollectChar = false;
-                continue;
-            }
-            if (ch == '\r') continue;
-            if (ch == '\n') break;
-            curVal.append(ch);
-        }
-        result.add(curVal.toString());
-        return result;
-    }
-
-    public static void main(String[] args) throws Exception {
-    }
-    */
 }
