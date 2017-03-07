086679832ecaa5342bf3ee61e5b8c7e9817cd6c7
chundrus <chundrus@rams.colostate.edu>
2017-03-06 20:31:41 -0700

    trying out stuff

--- src/main/java/com/csu2017sp314/dtr07/View/MapGUI.java
+++ src/main/java/com/csu2017sp314/dtr07/View/MapGUI.java
@@ -15,7 +15,8 @@ import javax.swing.*;
  */
 public class MapGUI {
     String filename;
-
+    public static JButton b1;
+    public static JLabel l1;
     public MapGUI(String filename) {
         this.filename = filename;
     }
@@ -24,7 +25,22 @@ public class MapGUI {
         File file = new File("./Colorado14ers.svg");
         BufferedImage image = ImageIO.read(file);
         JFrame f = new JFrame(); //creating instance of JFrame
+        f.setTitle("Background Colorado for JFrame");
+        f.setSize(400,400);
+        f.setLocationRelativeTo(null);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Closes app if window closes
+        f.setVisible(true);
+        f.setLayout(new BorderLayout());
+        JLabel background = new JLabel(new ImageIcon("./Colorado14ers.svg"));
+        f.add(background);
+        background.setLayout(new FlowLayout());
+        l1 = new JLabel("Here is a button");
+        b1 = new JButton("I am a button");
+        f.add(l1);
+        f.add(b1);
+
+
+        /*
         JLabel label = new JLabel(new ImageIcon(image));
         f.getContentPane().add(label);
         JButton b = new JButton("click"); //creating instance of JButton
@@ -34,5 +50,6 @@ public class MapGUI {
         f.setSize(1064, 780); //400 width and 500 height
         f.setLayout(null); //using no layout managers
         f.setVisible(true); //making the frame visible
+        */
     }
 }
