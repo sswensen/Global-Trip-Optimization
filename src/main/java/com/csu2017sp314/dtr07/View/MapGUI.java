5327979a4038dd3c1f8b755e5c7ac039ab5aae4e
SSwensen <scottswensen23@gmail.com>
2017-03-06 22:21:09 -0700

    Added working background with png

--- src/main/java/com/csu2017sp314/dtr07/View/MapGUI.java
+++ src/main/java/com/csu2017sp314/dtr07/View/MapGUI.java
@@ -3,6 +3,7 @@ package com.csu2017sp314.dtr07.View;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -21,17 +22,21 @@ public class MapGUI {
     }
 
     public static void main(String[] args) throws Exception {
-        File file = new File("./Colorado14ers.svg");
-        BufferedImage image = ImageIO.read(file);
-        JFrame f = new JFrame(); //creating instance of JFrame
+        JFrame f = new JFrame("TripCo"); //creating instance of JFrame
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Closes app if window closes
-        JLabel label = new JLabel(new ImageIcon(image));
-        f.getContentPane().add(label);
         JButton b = new JButton("click"); //creating instance of JButton
         b.setBounds(964, 0, 100, 40); //x axis, y axis, width, height
         f.add(b); //adding button in JFrame
-        f.pack();
-        f.setSize(1064, 780); //400 width and 500 height
+        f.setTitle("Background Color for JFrame");
+        f.setSize(400,400);
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        f.setLayout(new BorderLayout());
+        f.setContentPane(new JLabel(new ImageIcon("Colorado14ers.png")));
+        f.setLayout(new FlowLayout());
+        f.setSize(1063,779); //Refreshes window, needed or image doesn't appear
+        f.setSize(1064,780);
+        //f.pack(); //Will make everything MASSIVE
         f.setLayout(null); //using no layout managers
         f.setVisible(true); //making the frame visible
     }
