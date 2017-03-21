cb9c55e661d5014b520d15ff2db1f991ae90acf7
chundrus <chundrus@rams.colostate.edu>
2017-03-20 21:01:14 -0600

    tried some things, not really though.

--- src/main/java/com/csu2017sp314/dtr07/TripCo.java
+++ src/main/java/com/csu2017sp314/dtr07/TripCo.java
@@ -36,6 +36,9 @@ public class TripCo {
                 presenter.setThreeOpt(true);
                 //presenter.getThreeOpt();
                 break;
+            case "-f":
+                //
+                break;
             default:
                 break;
         }
