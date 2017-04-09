6cc28ee4000a25a31d492edabfbb94d539b67e20
chundrus <chundrus@rams.colostate.edu>
2017-04-09 17:18:31 -0600

    fixed the testing class for XMLBuilder

--- src/test/java/com/csu2017sp314/dtr07/View/XMLBuilderTest.java
+++ src/test/java/com/csu2017sp314/dtr07/View/XMLBuilderTest.java
@@ -10,11 +10,11 @@ import static org.junit.Assert.*;
 public class XMLBuilderTest {
     @Test
     public void addLeg() throws Exception {
-        //XMLBuilder xml1 = new XMLBuilder();
-        //XMLBuilder xml2 = new XMLBuilder();
-        //xml1.addLeg("", "", "", 1);
-        //xml2.addLeg("", "", "", 1);
-        //assertEquals(xml1.getXMLdoc().getDocumentElement().getFirstChild().getNodeName(), xml2.getXMLdoc().getDocumentElement().getFirstChild().getNodeName());
+        XMLBuilder xml1 = new XMLBuilder();
+        XMLBuilder xml2 = new XMLBuilder();
+        xml1.addLeg("","","","","","","","","","","","","","","","","","","","","","","","","",1,"");
+        xml2.addLeg("","","","","","","","","","","","","","","","","","","","","","","","","",1,"");
+        assertEquals(xml1.getXMLdoc().getDocumentElement().getFirstChild().getNodeName(), xml2.getXMLdoc().getDocumentElement().getFirstChild().getNodeName());
 
     }
 
