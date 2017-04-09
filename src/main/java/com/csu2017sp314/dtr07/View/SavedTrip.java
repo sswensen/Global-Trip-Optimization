6c475c4554b4597f89dd6bd6b7fd98a81e40e4a6
SSwensen <scottswensen23@gmail.com>
2017-04-08 22:58:44 -0600

    Updating savedtrip class

--- src/main/java/com/csu2017sp314/dtr07/View/SavedTrip.java
+++ src/main/java/com/csu2017sp314/dtr07/View/SavedTrip.java
@@ -1,5 +1,9 @@
 package com.csu2017sp314.dtr07.View;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -7,15 +11,23 @@ import java.util.ArrayList;
  */
 
 public class SavedTrip {
+    private final String myDriver = "com.mysql.jdbc.Driver";
+    //private final static String myUrl = "jdbc:mysql://faure.cs.colostate.edu/cs314"; //Original
+    private final String myUrl = "jdbc:mysql://127.0.0.1:3306/cs314"; //Using tunneling
+    private final String count = "SELECT COUNT(1) ";
+    private final String columns = "SELECT airports.id,airports.name,latitude,longitude,municipality,regions.name,countries.name,continents.name,airports.wikipedia_link,regions.wikipedia_link,countries.wikipedia_link ";
+    private final String continents = "FROM continents ";
+    private String where = "";
+    private final String join = "INNER JOIN countries ON countries.continent = continents.id "
+            + "INNER JOIN regions ON regions.iso_country = countries.code "
+            + "INNER JOIN airports ON airports.iso_region = regions.code ";
+    private final String limit = " LIMIT 100";
     private String name;
-    private ArrayList<String> ids;
-    private ArrayList<String> names;
     private ArrayList<GUILocation> locations;
 
-    public SavedTrip(String name, ArrayList<String> ids, ArrayList<String> names) {
+    public SavedTrip(String name, ArrayList<String> ids) {
         this.name = name;
-        this.ids = ids;
-        this.names = names;
+        findNamesOrIDs(ids);
     }
 
     public String getName() {
@@ -26,28 +38,16 @@ public class SavedTrip {
         this.name = name;
     }
 
-    public ArrayList<String> getIds() {
-        return ids;
-    }
-
-    public void setIds(ArrayList<String> ids) {
-        this.ids = ids;
-    }
-
     public ArrayList<String> getNames() {
         return names;
     }
 
-    public void setNames(ArrayList<String> names) {
-        this.names = names;
-    }
-
     public String getId(int index) {
-        return ids.get(index);
+        return locations.get(index).getId();
     }
 
     public String getName(int index) {
-        return names.get(index);
+        return locations.get(index).getName();
     }
 
     public ArrayList<GUILocation> getLocations() {
@@ -56,5 +56,52 @@ public class SavedTrip {
 
     public void setLocations(ArrayList<GUILocation> locations) {
         this.locations = locations;
+    }
+
+    public boolean containsName(String name) {
+        for(GUILocation n : locations) {
+            if(n.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void findNamesOrIDs(ArrayList<String> in) {
+        String whatYouWant = "name";
+        if(in.get(0).length() < 5) {
+            whatYouWant = "id";
+        }
+        where = "airports." + whatYouWant + in + "(";
+        for(int k = 0; k < in.size(); k++) {
+            where += "'" + in.get(k) + "', ";
+        }
+        where += "'" + in.get(in.size()-1) + "')";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cs314", "sswensen", "830534566");
+            Statement st = conn.createStatement();
+            System.out.println(columns + continents + join + where + limit);
+            ResultSet rs = st.executeQuery(columns + continents + join + where + limit);
+
+            while(rs.next()) {
+                ArrayList<Object> tempLoc = new ArrayList<>();
+                tempLoc.add(rs.getString(1));
+                tempLoc.add(rs.getString(2));
+                tempLoc.add(rs.getString(3));
+                tempLoc.add(rs.getString(4));
+                tempLoc.add(rs.getString(5));
+                tempLoc.add(rs.getString(6));
+                tempLoc.add(rs.getString(7));
+                tempLoc.add(rs.getString(8));
+                tempLoc.add(rs.getString(9));
+                tempLoc.add(rs.getString(10));
+                tempLoc.add(rs.getString(11));
+                locations.add(new GUILocation(tempLoc));
+            }
+        } catch(Exception e) {
+            System.err.println("Problem in MapGUI with database");
+        }
     }
 }
