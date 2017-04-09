7c700b47cf032d0d672fdac322f6cb8dfa3219e7
jzuniga77 <jzuniga7@rams.colostate.edu>
2017-04-09 17:36:48 -0600

    Fixed import order

--- src/main/java/com/csu2017sp314/dtr07/View/View.java
+++ src/main/java/com/csu2017sp314/dtr07/View/View.java
@@ -4,7 +4,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -18,6 +18,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import org.xml.sax.SAXException;
 //import org.w3c.dom.DOMImplementation;
 
 /**
