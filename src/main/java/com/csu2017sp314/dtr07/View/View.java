df3d61c4f6b111946f1afd9961f9fada9c719d43
jzuniga77 <jzuniga7@rams.colostate.edu>
2017-04-09 18:18:55 -0600

    Fixing issues

--- src/main/java/com/csu2017sp314/dtr07/View/View.java
+++ src/main/java/com/csu2017sp314/dtr07/View/View.java
@@ -1,5 +1,9 @@
 package com.csu2017sp314.dtr07.View;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.function.Consumer;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -8,10 +12,6 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.function.Consumer;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
