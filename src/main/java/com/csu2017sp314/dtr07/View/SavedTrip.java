cdc4ddc5c174d357d1598b1148643212c45d80b1
SSwensen <scottswensen23@gmail.com>
2017-04-09 18:08:24 -0600

    Refactor 1

--- src/main/java/com/csu2017sp314/dtr07/View/SavedTrip.java
+++ src/main/java/com/csu2017sp314/dtr07/View/SavedTrip.java
@@ -15,30 +15,32 @@ public class SavedTrip {
     //private final static String myUrl = "jdbc:mysql://faure.cs.colostate.edu/cs314"; //Original
     private final String myUrl = "jdbc:mysql://127.0.0.1:3306/cs314"; //Using tunneling
     private final String count = "SELECT COUNT(1) ";
-    private final String columns = "SELECT airports.id,airports.name,latitude,longitude,municipality,regions.name,countries.name,continents.name,airports.wikipedia_link,regions.wikipedia_link,countries.wikipedia_link ";
+    private final String columns = "SELECT airports.id,airports.name,latitude,longitude,municipality,"
+            + "regions.name,countries.name,continents.name,airports.wikipedia_link,regions.wikipedia_link,"
+            + "countries.wikipedia_link ";
     private final String continents = "FROM continents ";
-    private String where = "";
     private final String join = "INNER JOIN countries ON countries.continent = continents.id "
             + "INNER JOIN regions ON regions.iso_country = countries.code "
             + "INNER JOIN airports ON airports.iso_region = regions.code ";
     private final String limit = " LIMIT 333";
+    private String where = "";
     private String name;
     private ArrayList<GUILocation> locations = new ArrayList<>();
 
-    public SavedTrip(SavedTrip t1) {
+    SavedTrip() {
+        this.name = "untitiled";
+        locations = new ArrayList<>();
+
+    }
+
+    SavedTrip(SavedTrip t1) {
         name = t1.name;
         locations = new ArrayList<>(t1.getLocations());
     }
 
-    public SavedTrip(String name, ArrayList<String> ids) {
+    SavedTrip(String name, ArrayList<String> ids) {
         this.name = name;
         findNamesOrIDs(ids);
-    }
-
-    public SavedTrip() {
-        this.name = "untitiled";
-        locations = new ArrayList<>();
-
     }
 
     public String getName() {
@@ -112,32 +114,7 @@ public class SavedTrip {
 
     public void addName(String name) {
         where = "WHERE airports.name = \"" + name + "\"";
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cs314", "sswensen", "830534566");
-            Statement st = conn.createStatement();
-            System.out.println(columns + continents + join + where + limit);
-            ResultSet rs = st.executeQuery(columns + continents + join + where + limit);
-
-            while(rs.next()) {
-                ArrayList<Object> tempLoc = new ArrayList<>();
-                tempLoc.add(rs.getString(1));
-                tempLoc.add(rs.getString(2));
-                tempLoc.add("");
-                tempLoc.add("");
-                tempLoc.add(rs.getString(5));
-                tempLoc.add(rs.getString(6));
-                tempLoc.add(rs.getString(7));
-                tempLoc.add(rs.getString(8));
-                tempLoc.add(rs.getString(9));
-                tempLoc.add(rs.getString(10));
-                tempLoc.add(rs.getString(11));
-                locations.add(new GUILocation(tempLoc));
-            }
-        } catch(Exception e) {
-            System.err.println("Exception: " + e);
-            System.err.println("Problem in MapGUI with database");
-        }
+        fireQuery();
     }
 
     private void findNamesOrIDs(ArrayList<String> in) {
@@ -153,10 +130,14 @@ public class SavedTrip {
             where += "\"" + in.get(k) + "\", ";
         }
         where += "'" + in.get(in.size()-1) + "')";
+        fireQuery();
+    }
 
+    private void fireQuery() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cs314", "sswensen", "830534566");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cs314",
+                    "sswensen", "830534566");
             Statement st = conn.createStatement();
             System.out.println(columns + continents + join + where + limit);
             ResultSet rs = st.executeQuery(columns + continents + join + where + limit);
@@ -185,8 +166,8 @@ public class SavedTrip {
     @Override
     public String toString() {
         return "SavedTrip{" +
-                "name='" + name + '\'' +
-                "number of locations: " + locations.size() +
-                '}';
+                "name='" + name + '\''
+                + "number of locations: " + locations.size()
+                + '}';
     }
 }
