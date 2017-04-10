0838d787e09b483b010d7ec6dba4fabe272c4a3f
jzuniga77 <jzuniga7@rams.colostate.edu>
2017-04-09 18:12:36 -0600

    Fixing issues

--- src/main/java/com/csu2017sp314/dtr07/View/View.java
+++ src/main/java/com/csu2017sp314/dtr07/View/View.java
@@ -2,7 +2,6 @@ package com.csu2017sp314.dtr07.View;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -15,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
