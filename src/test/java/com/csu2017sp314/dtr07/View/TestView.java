30ace579f052c5af0b457cb2addd35e267dcf1bd
chundrus <chundrus@rams.colostate.edu>
2017-04-10 23:52:03 -0600

    Changed small detail

--- src/test/java/com/csu2017sp314/dtr07/View/TestView.java
+++ src/test/java/com/csu2017sp314/dtr07/View/TestView.java
@@ -48,110 +48,110 @@ public class TestView {
 
     @Test
     public void initializeTrip() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addLeg("1","Denver International Airport",Double.toString(39.861698150635), Double.toString(-104.672996521),"","","","","","","","","","","","","","","","","","","","","",5000,"");
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addLeg("1","Denver International Airport",Double.toString(39.861698150635), Double.toString(-104.672996521),"","","","","","","","","","","","","","","","","","","","","",5000,"");
         assertEquals(one.getXMLdoc().getDocumentElement().getFirstChild().getFirstChild().getNextSibling().getFirstChild().getNodeValue(), v.getXMLdoc().getDocumentElement().getFirstChild().getFirstChild().getNextSibling().getFirstChild().getNodeValue());
     }
 
     @Test
     public void addLeg() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addLeg("1","Denver International Airport",Double.toString(39.861698150635), Double.toString(-104.672996521),"","","","","","","","","","","","","","","","","","","","","",5000,"");
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addLeg("1","Denver International Airport",Double.toString(39.861698150635), Double.toString(-104.672996521),"","","","","","","","","","","","","","","","","","","","","",5000,"");
         assertEquals(one.getXMLdoc().getDocumentElement().getFirstChild().getFirstChild().getNextSibling().getFirstChild().getNodeValue(), v.getXMLdoc().getDocumentElement().getFirstChild().getFirstChild().getNextSibling().getFirstChild().getNodeValue());
     }
 
     @Test
     public void addFinalLeg() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addLeg("1","Denver International Airport",Double.toString(39.861698150635), Double.toString(-104.672996521),"","","","","","","","","","","","","","","","","","","","","",5000,"");
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addLeg("1","Denver International Airport",Double.toString(39.861698150635), Double.toString(-104.672996521),"","","","","","","","","","","","","","","","","","","","","",5000,"");
         assertEquals(one.getXMLdoc().getDocumentElement().getFirstChild().getFirstChild().getNextSibling().getFirstChild().getNodeValue(), v.getXMLdoc().getDocumentElement().getFirstChild().getFirstChild().getNextSibling().getFirstChild().getNodeValue());
     }
 
     @Test
     public void addLine() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addLine(-109,41,-102,37,"1", false);
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addLine(-109,41,-102,37,"1", false);
         assertEquals(one.getSVGdoc().getDocumentElement().getFirstChild().getNodeName(), v.getSVGdoc().getDocumentElement().getFirstChild().getNodeName());
     }
 
     @Test
     public void addDistance() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addDistance(-109,41,-102,37,1, "2", true);
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addDistance(-109,41,-102,37,1, "2", true);
         assertEquals(one.getSVGdoc().getDocumentElement().getFirstChild().getNodeName(), v.getSVGdoc().getDocumentElement().getFirstChild().getNodeName());
     }
 
     @Test
     public void addCityNameLabel() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addCityNameLabel(-109,41,"Denver");
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addCityNameLabel(-109,41,"Denver");
         assertEquals(one.getSVGdoc().getDocumentElement().getFirstChild().getNodeName(), v.getSVGdoc().getDocumentElement().getFirstChild().getNodeName());
     }
 
     @Test
     public void addIDLabel() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addCityNameLabel(-109, 41, "Denver");
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addCityNameLabel(-109, 41, "Denver");
         assertEquals(one.getSVGdoc().getDocumentElement().getFirstChild().getNodeName(), v.getSVGdoc().getDocumentElement().getFirstChild().getNodeName());
     }
 
     @Test
     public void addHeader() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addHeader("Denver");
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addHeader("Denver");
         assertEquals(one.getSVGdoc().getDocumentElement().getFirstChild().getNodeName(), v.getSVGdoc().getDocumentElement().getFirstChild().getNodeName());
     }
 
     @Test
     public void addFooter() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addFooter(9999);
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addFooter(9999);
         assertEquals(one.getSVGdoc().getDocumentElement().getFirstChild().getNodeName(), v.getSVGdoc().getDocumentElement().getFirstChild().getNodeName());
     }
 
     @Test
     public void addBorders() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addBorders();
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addBorders();
         assertEquals(one.getSVGdoc().getDocumentElement().getFirstChild().getNodeName(), v.getSVGdoc().getDocumentElement().getFirstChild().getNodeName());
     }
 
     @Test
     public void finalizeTrip() throws Exception {
-        v.initializeTrip("S3/world2.svg");
+        v.initializeTrip("S3/World2.svg");
         v.addLeg("1", "Denver International Airport", Double.toString(39.861698150635), Double.toString(-104.672996521), "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 5000, "");
         View one = new View();
-        one.initializeTrip("S3/world2.svg");
+        one.initializeTrip("S3/World2.svg");
         one.addLeg("1", "Denver International Airport", Double.toString(39.861698150635), Double.toString(-104.672996521), "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 5000, "");
         assertEquals(one.getXMLdoc().getDocumentElement().getFirstChild().getFirstChild().getNextSibling().getFirstChild().getNodeValue(), v.getXMLdoc().getDocumentElement().getFirstChild().getFirstChild().getNextSibling().getFirstChild().getNodeValue());
     }
