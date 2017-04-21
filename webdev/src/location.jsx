1097778f40f2b4ac410f2c4e1b1e9eb01bdd6b2c
SSwensen <scottswensen23@gmail.com>
2017-04-21 12:23:27 -0600

    Updated css for locations

--- webdev/src/location.jsx
+++ webdev/src/location.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 let Location = ({name, municipality, region, country, select}) => <li className="location">
-    <h5>{name}</h5>
-    <div className="other-info">
-        {municipality}, {region}, {country}
+    <div>
+        <div className="add-button-div">
+            <button className="add-button" onClick={select}>Add</button>
+        </div>
+        <div className="location-info">
+            <h5>{name}</h5>
+            <div className="other-info">
+                {municipality}, {region}, {country}
+            </div>
+        </div>
     </div>
-    <button onClick={select}>Select Me</button>
 </li>;
 
 export default Location;
