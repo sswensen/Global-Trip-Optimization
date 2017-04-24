4f2cbf741d4f5f76aa08f712868d468ba5163ade
SSwensen <scottswensen23@gmail.com>
2017-04-24 01:55:36 -0600

    Added total trip distance and distance calculation, insertion where best

--- webdev/src/App/app.jsx
+++ webdev/src/App/app.jsx
@@ -7,10 +7,10 @@ let Sel = ({locations}) => <div>
     {locations.map(l => <li key={l.id}>{l.name}</li>)}
 </div>
 /*
-<Sel locations={Object.values(this.state.selectedLocations)}/>
+ <Sel locations={Object.values(this.state.selectedLocations)}/>
  */
 
- class App extends React.Component {
+class App extends React.Component {
     constructor(props) {
         super(props); // this is required
         this.state = {
@@ -25,36 +25,56 @@ let Sel = ({locations}) => <div>
             <div className="planning-stuff">
                 <LocationSearch selectLocation={this.selectLocation.bind(this)}/>
                 <TripPlanner setLocations={Object.values(this.state.selectedLocations)}
-                removeLocation={this.removeLocation.bind(this)} saveTrip={this.saveTrip.bind(this)}
-                clear={this.clearSelectedLocations.bind(this)}
-                tripDistance={this.state.tripDistance} />
+                             removeLocation={this.removeLocation.bind(this)} saveTrip={this.saveTrip.bind(this)}
+                             clear={this.clearSelectedLocations.bind(this)}
+                             tripDistance={this.state.tripDistance}/>
             </div>
             <button className="testing" onClick={this.test.bind(this)}>test</button>
-            <TripMap ref={instance => { this.child = instance; }}
+            <TripMap ref={instance => {
+                this.child = instance;
+            }}
                      selectedLocations={Object.values(this.state.selectedLocations)}/>
         </div>
     }
 
-
-
     selectLocation(loc) {
+        console.log("Adding location with name", loc.name);
         let currentLocations = Object.values(this.state.selectedLocations);
-        for(let i = 0; i < currentLocations.length; ++i) {
-            console.log(currentLocations[i].name);
-            //TODO
-            //Figure out where the best place to put the location is
-            //Need to also measure between the first and the last locations
-            //This will account for nearestNeighbor
+        let numLocs = currentLocations.length;
+        let bestDist = 9999999;
+        let whereToInsert = 0;
+        let totalTripDistance = 0;
+        if (numLocs > 2) {
+            //console.log("Now calculating distances for insertion")
+            for (let i = 0; i < numLocs; ++i) {
+                //console.log(currentLocations[i].name);
+                //console.log(currentLocations[(i + 1) % (numLocs - 1)].name);
+                //TODO
+                //Figure out where the best place to put the location is
+                //Need to also measure between the first and the last locations
+                //This will account for nearestNeighbor
+                let loc1 = currentLocations[i];
+                let loc2 = currentLocations[(i + 1) % (numLocs - 1)];
+                let dist = this.distanceBetweenCoords(loc1.lat, loc1.lon, loc2.lat, loc2.lon);
+                totalTripDistance += dist;
+                console.log("Distance between", loc1.name, "and", loc2.name, "is", dist);
+                if (dist < bestDist) {
+                    whereToInsert = i;
+                }
+            }
         }
+        //TODO can I insert at an index here?
         let obj = {};
         obj[loc.id] = loc;
         let newMap = Object.assign({},
             this.state.selectedLocations,
             obj);
         this.setState({
-            selectedLocations: newMap
+            selectedLocations: newMap,
+            tripDistance: totalTripDistance,
         });
         this.updateMarkers(newMap);
+        //console.log(Object.values(newMap));
         //Need to update total trip distance here
     }
 
@@ -80,7 +100,7 @@ let Sel = ({locations}) => <div>
     }
 
     clearSelectedLocations() {
-        this.setState ({
+        this.setState({
             selectedLocations: {}
         });
         this.updateMarkers({});
@@ -90,8 +110,26 @@ let Sel = ({locations}) => <div>
         this.child.updateMarkers(map); //double comp callback
     }
 
+    distanceBetweenCoords(lat1, lon1, lat2, lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
+        var dLon = this.deg2rad(lon2 - lon1);
+        var a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c; // Distance in km
+        return d * 0.621371; //Now returns in miles
+    }
+
+    deg2rad(deg) {
+        return deg * (Math.PI / 180)
+    }
+
     test() {
-        console.log("[app]: selectedLocations:",this.state.selectedLocations," \n[app]: savedTrips:",this.state.savedTrips);
+        console.log("[app]: selectedLocations:", this.state.selectedLocations, " \n[app]: savedTrips:", this.state.savedTrips);
     }
 
     isDead() {
