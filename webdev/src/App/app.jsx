3308778a4867c7ec9e3fa38029460a8c1295b631
SSwensen <scottswensen23@gmail.com>
2017-04-25 16:16:06 -0600

    Finally pops out

--- webdev/src/App/app.jsx
+++ webdev/src/App/app.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import LocationSearch from './LocationSearch/location_search.jsx';
 import TripMap from './TripMap/trip_map.jsx';
 import TripPlanner from "./TripPlanner/trip_planner.jsx";
+import LeftMenu from './leftMenu.jsx';
 
 let Sel = ({locations}) => <div>
     {locations.map(l => <li key={l.id}>{l.name}</li>)}
-</div>
+</div>;
 /*
  <Sel locations={Object.values(this.state.selectedLocations)}/>
  */
@@ -18,13 +19,15 @@ class App extends React.Component {
             savedTrips: {},
             tripDistance: 0,
             sortedLocationIds: [],
+            leftMenu: false,
+            rightMenu: false,
         }
     }
 
     render() {
         return <div>
-            <div className="planning-stuff">
-                <LocationSearch selectLocation={this.selectLocation.bind(this)}/>
+            <div id="main" className="planning-stuff">
+                <LeftMenu leftMenu={this.state.leftMenu} selectLocation={this.selectLocation.bind(this)}/>
                 <TripPlanner setLocations={Object.values(this.state.selectedLocations)}
                              removeLocation={this.removeLocation.bind(this)} saveTrip={this.saveTrip.bind(this)}
                              clear={this.clearSelectedLocations.bind(this)}
@@ -38,6 +41,7 @@ class App extends React.Component {
                      sortedLocationIds={this.state.sortedLocationIds}
             />
             <button className="testing" onClick={this.test.bind(this)}>test</button>
+            <span className="left-menu-button" onClick={this.openNav.bind(this)}>&#9776; open</span>
         </div>
     }
 
@@ -46,7 +50,7 @@ class App extends React.Component {
         let currentLocations = Object.values(this.state.selectedLocations);
         let numLocs = currentLocations.length;
         let tempLocationList = [];
-        for(let i = 0; i < numLocs; i++) {
+        for (let i = 0; i < numLocs; i++) {
             let currentId = this.state.sortedLocationIds[i];
             let currentLocation = this.searchSelectedLocationsWithId(currentId);
             tempLocationList.push(currentLocation)
@@ -58,7 +62,7 @@ class App extends React.Component {
             let bestDist = 9999999;
             let minusDist = 0;
             let plusDist = 0;
-            console.log("Now calculating distances for insertion")
+            console.log("Now calculating distances for insertion");
             for (let i = 0; i < numLocs; i++) {
                 //console.log(currentLocations[i].name);
                 //console.log(currentLocations[(i + 1) % (numLocs - 1)].name);
@@ -84,39 +88,39 @@ class App extends React.Component {
             totalDist = totalDist - minusDist + plusDist;
         }
         let newSortedLocationIds = this.state.sortedLocationIds;
-        newSortedLocationIds.splice(whereToInsert+1, 0, loc.id);
-        this.setState ({
+        newSortedLocationIds.splice(whereToInsert + 1, 0, loc.id);
+        this.setState({
             sortedLocationIds: newSortedLocationIds,
             tripDistance: totalDist
         });
-        console.log("Should be inserted at index:", whereToInsert+1, this.state.sortedLocationIds);
+        console.log("Should be inserted at index:", whereToInsert + 1, this.state.sortedLocationIds);
 
         /*let totalTripDistance = 0;
-        if (numLocs > 1) {
-            //console.log("Now calculating distances for insertion")
-            for (let i = 0; i < numLocs; i++) {
-                //console.log(currentLocations[i].name);
-                //console.log(currentLocations[(i + 1) % (numLocs - 1)].name);
-                //TODO
-                //Figure out where the best place to put the location is
-                //Need to also measure between the first and the last locations
-                //This will account for nearestNeighbor
-                let loc1 = currentLocations[i];
-                let loc2 = currentLocations[(i + 1) % (numLocs)];
-                let dist = this.distanceBetweenCoords(loc1.lat, loc1.lon, loc2.lat, loc2.lon);
-                totalTripDistance += dist; //TODO this doesnt work because we are not calculating the entire trip yet
-                console.log("Distance between", loc1.name, "and", loc2.name, "is", dist);
-                if (dist < bestDist) {
-                    whereToInsert = i;
-                }
-            }
-        } else if(numLocs === 2) {
-            let loc1 = currentLocations[0];
-            let loc2 = currentLocations[1];
-            this.setState({
-                tripDistance: this.distanceBetweenCoords(loc1.lat, loc1.lon, loc2.lat, loc2.lon),
-            });
-        }*/
+         if (numLocs > 1) {
+         //console.log("Now calculating distances for insertion")
+         for (let i = 0; i < numLocs; i++) {
+         //console.log(currentLocations[i].name);
+         //console.log(currentLocations[(i + 1) % (numLocs - 1)].name);
+         //TODO
+         //Figure out where the best place to put the location is
+         //Need to also measure between the first and the last locations
+         //This will account for nearestNeighbor
+         let loc1 = currentLocations[i];
+         let loc2 = currentLocations[(i + 1) % (numLocs)];
+         let dist = this.distanceBetweenCoords(loc1.lat, loc1.lon, loc2.lat, loc2.lon);
+         totalTripDistance += dist; //TODO this doesnt work because we are not calculating the entire trip yet
+         console.log("Distance between", loc1.name, "and", loc2.name, "is", dist);
+         if (dist < bestDist) {
+         whereToInsert = i;
+         }
+         }
+         } else if(numLocs === 2) {
+         let loc1 = currentLocations[0];
+         let loc2 = currentLocations[1];
+         this.setState({
+         tripDistance: this.distanceBetweenCoords(loc1.lat, loc1.lon, loc2.lat, loc2.lon),
+         });
+         }*/
         let obj = {};
         obj[loc.id] = loc;
         let newMap = Object.assign({},
@@ -130,8 +134,8 @@ class App extends React.Component {
 
     searchSelectedLocationsWithId(id) {
         let locations = Object.values(this.state.selectedLocations);
-        for(let i = 0; i < locations.length; i++) {
-            if(id === locations[i].id) {
+        for (let i = 0; i < locations.length; i++) {
+            if (id === locations[i].id) {
                 return locations[i];
             }
         }
@@ -153,7 +157,7 @@ class App extends React.Component {
         }
         this.setState({
             sortedLocationIds: tempSortedLocations,
-        })
+        });
         this.updateMarkers(newMap, tempSortedLocations);
     }
 
@@ -181,16 +185,16 @@ class App extends React.Component {
     }
 
     distanceBetweenCoords(lat1, lon1, lat2, lon2) {
-        var R = 6371; // Radius of the earth in km
-        var dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
-        var dLon = this.deg2rad(lon2 - lon1);
-        var a =
+        let R = 6371; // Radius of the earth in km
+        let dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
+        let dLon = this.deg2rad(lon2 - lon1);
+        let a =
             Math.sin(dLat / 2) * Math.sin(dLat / 2) +
             Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
             Math.sin(dLon / 2) * Math.sin(dLon / 2)
         ;
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c; // Distance in km
+        let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        let d = R * c; // Distance in km
         return d * 0.621371; //Now returns in miles
     }
 
@@ -198,11 +202,30 @@ class App extends React.Component {
         return deg * (Math.PI / 180)
     }
 
+    openNav() {
+        //document.getElementById("mySidenav").style.width = "250px";
+        //document.getElementById("main").style.marginLeft = "250px";
+        console.log("Left now true");
+        this.setState({
+            leftMenu: true,
+        });
+    }
+
+    closeNav() {
+        //document.getElementById("mySidenav").style.width = "0";
+        //document.getElementById("main").style.marginLeft = "0";
+        console.log("Left now false");
+        this.setState({
+            leftMenu: false,
+        });
+    }
+
     test() {
+        console.log("leftMenu:", this.state.leftMenu);
         console.log("[app]: selectedLocations:", this.state.selectedLocations, " \n[app]: savedTrips:", this.state.savedTrips, " \n[app]: tripDistance:", this.state.tripDistance, " \n[app]: sortedLocationIds:", this.state.sortedLocationIds);
     }
 
-    isDead() {
+    static isDead() {
         return true;
     }
 }
