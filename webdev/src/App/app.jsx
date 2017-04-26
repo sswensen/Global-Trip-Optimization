00a73c2a5306e2f082b8c0f9db2a024bc1ab329b
SSwensen <scottswensen23@gmail.com>
2017-04-25 19:05:10 -0600

    Updates

--- webdev/src/App/app.jsx
+++ webdev/src/App/app.jsx
@@ -73,7 +73,7 @@ class App extends React.Component {
                 let dist2 = this.distanceBetweenCoords(loc.lat, loc.lon, loc2.lat, loc2.lon);
                 let dist = dist1 + dist2;
                 totalDist += originalDist;
-                console.log("Distance added between", loc1.name, "and", loc2.name, "is", dist);
+                //console.log("Distance added between", loc1.name, "and", loc2.name, "is", dist);
                 if (dist < bestDist) {
                     whereToInsert = i;
                     bestDist = dist;
@@ -89,34 +89,7 @@ class App extends React.Component {
             sortedLocationIds: newSortedLocationIds,
             tripDistance: totalDist
         });
-        console.log("Should be inserted at index:", whereToInsert+1, this.state.sortedLocationIds);
-
-        /*let totalTripDistance = 0;
-        if (numLocs > 1) {
-            //console.log("Now calculating distances for insertion")
-            for (let i = 0; i < numLocs; i++) {
-                //console.log(currentLocations[i].name);
-                //console.log(currentLocations[(i + 1) % (numLocs - 1)].name);
-                //TODO
-                //Figure out where the best place to put the location is
-                //Need to also measure between the first and the last locations
-                //This will account for nearestNeighbor
-                let loc1 = currentLocations[i];
-                let loc2 = currentLocations[(i + 1) % (numLocs)];
-                let dist = this.distanceBetweenCoords(loc1.lat, loc1.lon, loc2.lat, loc2.lon);
-                totalTripDistance += dist; //TODO this doesnt work because we are not calculating the entire trip yet
-                console.log("Distance between", loc1.name, "and", loc2.name, "is", dist);
-                if (dist < bestDist) {
-                    whereToInsert = i;
-                }
-            }
-        } else if(numLocs === 2) {
-            let loc1 = currentLocations[0];
-            let loc2 = currentLocations[1];
-            this.setState({
-                tripDistance: this.distanceBetweenCoords(loc1.lat, loc1.lon, loc2.lat, loc2.lon),
-            });
-        }*/
+        //console.log("Should be inserted at index:", whereToInsert+1, this.state.sortedLocationIds);
         let obj = {};
         obj[loc.id] = loc;
         let newMap = Object.assign({},
