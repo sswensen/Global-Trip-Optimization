03b36a86fd0f8bf4a1b93106045e1d0c04a089d1
SSwensen <scottswensen23@gmail.com>
2017-04-28 13:49:42 -0600

    Trips update from server on load

--- webdev/src/App/app.jsx
+++ webdev/src/App/app.jsx
@@ -14,7 +14,7 @@ let Sel = ({locations}) => <div>
 class App extends React.Component {
     constructor(props) {
         super(props); // this is required
-        //this.getTripsFromServer();
+        this.getTripsFromServer();
         this.state = {
             name: "",
             selectedLocations: {},
