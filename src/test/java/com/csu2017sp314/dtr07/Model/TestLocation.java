3f70a0647fe7b4630b8da0293b654fc8925cc47c
jzuniga77 <jzuniga7@rams.colostate.edu>
2017-05-01 23:55:00 -0600

    Added tests to TestLocation

--- src/test/java/com/csu2017sp314/dtr07/Model/TestLocation.java
+++ src/test/java/com/csu2017sp314/dtr07/Model/TestLocation.java
@@ -15,6 +15,9 @@ public class TestLocation {
     }
 
     Location one = (new Location("0", "A", "39.701698303200004", "-104.751998901", "Aurora", "Colorado", "United States", "North America", "http://en.wikipedia.org/wiki/Buckley_Air_Force_Base", "http://en.wikipedia.org/wiki/Colorado", "http://en.wikipedia.org/wiki/United_States"));
+    Location two = (new Location("1", "B", "-39.701698303200004", "104.751998901", "Fort Collins", "Colorado", "United States", "North America", "http://en.wikipedia.org/wiki/Buckley_Air_Force_Base", "http://en.wikipedia.org/wiki/Colorado", "http://en.wikipedia.org/wiki/United_States"));
+    Location equal = (new Location("0", "A", "39.701698303200004", "-104.751998901", "Aurora", "Colorado", "United States", "North America", "http://en.wikipedia.org/wiki/Buckley_Air_Force_Base", "http://en.wikipedia.org/wiki/Colorado", "http://en.wikipedia.org/wiki/United_States"));
+
 
     @Test
     public void getId() throws Exception {
@@ -45,17 +48,44 @@ public class TestLocation {
     }
 
     @Test
-    public void setLat() throws Exception {
-        //one.setLat("40°28'20\" N");
-    }
-
-    @Test
     public void getLon() throws Exception {
         assertEquals(-104.751998901, one.getLon(), 0);
     }
 
     @Test
-    public void setLon() throws Exception {
-        //one.setLat("3106°47'35\" W");
+    public void distance() throws Exception {
+        assertEquals(10875.0, one.distance(two), 0);
+    }
+
+    @Test
+    public void getTableIndex() {
+        assertEquals(0, one.getTableIndex());
+    }
+
+    @Test
+    public void setTableIndex() {
+        one.setTableIndex(45);
+        assertEquals(45, one.getTableIndex());
+    }
+
+    @Test
+    public void getMunicipality() {
+        assertEquals("Aurora", one.getMunicipality());
+    }
+
+    @Test
+    public void getRegion() {
+        assertEquals("Colorado", one.getRegion());
+    }
+
+    @Test public void getCountry() {
+        assertEquals("United States", one.getCountry());
+    }
+
+    @Test
+    public void equals() {
+        assertEquals(true, one.equals(one));
+        assertEquals(false, one.equals(two));
+        assertEquals(true, one.equals(equal));
     }
 }
