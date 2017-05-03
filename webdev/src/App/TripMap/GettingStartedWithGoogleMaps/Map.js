3deab9fd340eb3d13734754e3e91aa72db156c2c
SSwensen <scottswensen23@gmail.com>
2017-05-03 16:09:20 -0600

    10

--- webdev/src/App/TripMap/GettingStartedWithGoogleMaps/Map.js
+++ webdev/src/App/TripMap/GettingStartedWithGoogleMaps/Map.js
@@ -24,7 +24,7 @@ import {
 const GettingStartedGoogleMap = withGoogleMap(props => (
     <GoogleMap
         ref={props.onMapLoad}
-        defaultZoom={2}
+        defaultZoom={10}
         defaultCenter={{lat: 39.7392, lng: -104.9903}}
     >
         {props.children}
